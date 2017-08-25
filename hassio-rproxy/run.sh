533f274d1a1dec0b39ca1b9647771f61e8d84a9d
besmith2 <bruce87.smith@gmail.com>
2017-08-25 16:18:10 -0400

    Looks to be working.

--- hassio-rproxy/run.sh
+++ hassio-rproxy/run.sh
@@ -3,39 +3,6 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-
-ADDRESS=$(jq --raw-output ".address" $CONFIG_PATH)
-PORT=$(jq --raw-output ".port" $CONFIG_PATH)
-OPTIONS=$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)
-TIVO=$(jq --raw-output ".tivo" $CONFIG_PATH)
-
-export ARGS=""
-
-if [[ ! -z $(jq --raw-output ".address" $CONFIG_PATH) ]]; then
-    ARGS="$ARGS -a $(jq --raw-output ".address" $CONFIG_PATH)"
-fi
-
-if [[ ! -z $(jq --raw-output ".port" $CONFIG_PATH) ]]; then
-    ARGS="$ARGS -p $(jq --raw-output ".port" $CONFIG_PATH)"
-fi
-
-if [[ ! -z $(jq --raw-output ".tivo" $CONFIG_PATH) ]]; then
-    ARGS=" -tivo $(jq --raw-output ".tivo" $CONFIG_PATH)"
-fi
-
-if [[ ! -z $(jq --raw-output ".commandlineoptions" $CONFIG_PATH) ]]; then
-    ARGS="$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)"
-fi
-
-
-
-
-echo "$ARGS"
-
-echo CONFIG_PATH
-echo CONFIG_DIR
-echo "$ADDRESS"
-echo $(ls)
-echo $(ls rproxy/)
-echo /rproxy/rproxy.py "$ARGS"
-python2 /rproxy/rproxy.py "$ARGS"
+echo Loading rproxy ...
+echo Running with the following parameters /rproxy/rproxy.py "$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)"
+python2 /rproxy/rproxy.py "$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)"
