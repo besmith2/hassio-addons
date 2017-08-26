82da06f54756d97ff8104ed73d25da55b6c0ffe2
besmith2 <bruce87.smith@gmail.com>
2017-08-26 11:31:07 -0400

    options updated

--- hassio-rproxy/run.sh
+++ hassio-rproxy/run.sh
@@ -6,6 +6,4 @@ CONFIG_DIR=/config/hadaemon
 echo Loading rproxy ...
 echo Running with the following parameters /rproxy/rproxy.py "$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)"
 
-#python2 /rproxy/rproxy.py -h
-python2 /rproxy/rproxy.py -a 192.168.0.112 -v -f 
-#python2 /rproxy/rproxy.py "$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)"
+python2 /rproxy/rproxy.py "$(jq --raw-output ".commandlineoptions" $CONFIG_PATH)"
